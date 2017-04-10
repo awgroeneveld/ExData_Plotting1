4a818e0459e2ecad3361f9fd039ac7a8c4963925
Aart-Wichert Groeneveld <awgroeneveld@gmail.com>
2017-04-10 23:06:42 +0200

    comments

--- readFile.R
+++ readFile.R
@@ -4,6 +4,7 @@ library("dplyr")
 readHHPower<-function(fileName){
   # read file and filter for specific dates
   hhpc<-fread(fileName, na.strings = c("NA","?")) %>% filter(Date=="1/2/2007" | Date=="2/2/2007")
+  # converts the date/time columns to a POSIXlt type
   hhpc$DateTime<-(strptime(paste(hhpc$Date,hhpc$Time), "%d/%m/%Y %H:%M:%S"))
   hhpc
 }
